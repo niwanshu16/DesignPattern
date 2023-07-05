0e6f259c68b388fa69358cf205c8222779468fde
Niwanshu Maheshwari <niwanshu16@gmail.com>
2023-07-05 06:55:56 +0530

    Add some comment

--- CommandPattern/SortingStrategyWithCommand/TestSortingCommand.java
+++ CommandPattern/SortingStrategyWithCommand/TestSortingCommand.java
@@ -1,9 +1,11 @@
 package DesignPattern.CommandPattern.SortingStrategyWithCommand;
 
+//Client
 public class TestSortingCommand {
     
     public static void main(String []args) {
 
+        //Receiver
         SortManagerImpl sortManager = new SortManagerImpl();
         int arr[] = {5,3,2,2};
         
@@ -13,12 +15,16 @@ public class TestSortingCommand {
         SortingStrategy quickSortingStrategy = new QuickSortStrategy();
         sortManager.setSortingStrategy(quickSortingStrategy);
         
-        SortCommand sortCommand = new SortCommand(sortManager,arr);
+        //Concreete Command
+        Command sortCommand = new SortCommand(sortManager,arr);
 
+        //Invoker
         SortInvoker sortInvoker = new SortInvoker();
         sortInvoker.setCommand(sortCommand);
         sortInvoker.executeCommand();
 
+        //The object SortInvoker which call the functionality and the object SortManager which executes the 
+        // function are different and hence decopuled.
         sortManager.setSortingStrategy(bubbSortingStrategy);
         sortCommand = new SortCommand(sortManager,arr);
         sortInvoker.setCommand(sortCommand);
